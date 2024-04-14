e100760708e2b2bf681abbdd4c2a62d96ddb9099
Duy Khoa <npduykhoa@gmail.com>
2024-04-14 10:09:47 +0700

    Using passport jwt to login user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,27 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Render,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
-  @Get()
-  @Render('home')
-  getHello() {
-    console.log('>>> check port =', this.configService.get<string>('PORT'));
-
-    const message = this.appService.getHello();
-
-    return { message };
-    // return 'this.appService.getHello()';
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   //username & pass are two params returned by passport
   async validateUser(username: string, pass: string): Promise<any> {
@@ -16,5 +20,12 @@ export class AuthService {
     }
 
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
